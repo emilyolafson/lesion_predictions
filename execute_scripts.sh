4a5620fefbc14ff0c39df0110b10a139925bd723
Emily Olafson <emily.olafson@mail.mcgill.ca>
2022-12-12 13:33:47 -0500

    remove old files

--- execute_scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# python3 parse_args.py --help
-
-csv_path='/home/ubuntu/enigma/Behaviour_Information_ALL_April7_2022_sorted_CST_12_ll_slnm.csv'
-nemo_path='/home/ubuntu/enigma/lesionmasks/'
-
-python3 parse_args.py --csv_path $csv_path --nemo_path $nemo_path --chaco_types 'chacovol' --atlases 'shen268' --lesionload_types 'M1' --verbose False --site_colname 'SITE' --chronicity_colname 'CHRONICITY' --model_specified 'none'
-
